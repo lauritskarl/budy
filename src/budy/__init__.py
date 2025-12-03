748ebe86db48f304d20b592e716f2a80ba180ba5
Karl Hans Laurits <karl@laurits.dev>
2025-12-03 12:39:16 +0200

    chore: move reports to their own directory

--- src/budy/__init__.py
+++ src/budy/__init__.py
@@ -3,7 +3,7 @@ from typer import Typer
 
 from budy.budgets import app as budgets_app
 from budy.database import engine
-from budy.report import app as report_app
+from budy.reports import app as reports_app
 from budy.transactions import app as transactions_app
 
 SQLModel.metadata.create_all(engine)
@@ -15,7 +15,7 @@ app = Typer(
 
 app.add_typer(transactions_app, name="transactions")
 app.add_typer(budgets_app, name="budgets")
-app.add_typer(report_app, name="report")
+app.add_typer(reports_app, name="reports")
 
 
 @app.callback()
